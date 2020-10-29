88846c837ec4e8d8877d8e8ce006d039f68b3fed
sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>
2020-10-29 22:46:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ def main(epoch, save_path, load_path, samples, data_file_path, batch_size):
 
 			# generator 진행
 			if (count > 0 and count % 1000 == 0) or (len(data) < batch_size):
-				sent = sample_sequence(model.to("cpu"), tok, vocab, sent="사랑", text_size=100, temperature=0.7, top_p=0.8, top_k=40)
+				sent = sample_sequence(model.to("cpu"), tok, vocab, sent="우리", text_size=100, temperature=0.7, top_p=0.8, top_k=40)
 				sent = sent.replace("<unused0>", "\n") # 비효율적이지만 엔터를 위해서 등장
 				sent = auto_enter(sent)
 				print(sent)
